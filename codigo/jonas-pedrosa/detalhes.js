30b2fb5c19f323f8c4b15a9c1e29da602b25d242
JPedrosa16 <jlfpedrosa@sga.pucminas.br>
2025-05-27 10:08:08 -0300

    KSI
    KSKS

---
+++ codigo/jonas-pedrosa/detalhes.js
@@ -0,0 +1,22 @@
+{
+  "clientes": [
+    {
+      "id": 1,
+      "nome": "Leanne Graham",
+      "cidade": "Belo Horizonte",
+      "categoria": "preferenciais",
+      "email": "leanne@bmail.com",
+      "telefone": "31-99736-8031",
+      "website": "hildegard.org"
+    },
+    {
+      "id": 2,
+      "nome": "Ervin Howell",
+      "cidade": "São Paulo",
+      "categoria": "comuns",
+      "email": "ervin@smail.com",
+      "telefone": "11-98765-4321",
+      "website": "anastasia.net"
+    }
+  ]
+}
