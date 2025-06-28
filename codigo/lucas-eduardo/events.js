8584a9b46538f086247ccfe453630732537fbde3
lucassts5 <lucas123edu78@gmail.com>
2025-06-28 00:35:17 -0300

    teste

--- codigo/lucas-eduardo/events.js
+++ codigo/lucas-eduardo/events.js
@@ -1,15 +1,19 @@
-// url JSON de eventos.
 fetch("eventos.json")
   .then(response => response.json())
   .then(data => {
-    
-    const evento = data[0];
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    const evento = data.find(e => e.id == id);
 
-    document.getElementById("tituloEvento").textContent = evento.titulo;
-    document.getElementById("localEvento").textContent = evento.local;
-    document.getElementById("precoEvento").textContent = evento.preco;
-    document.getElementById("tipoEvento").textContent = evento.tipo;
-    document.getElementById("artistasEvento").textContent = evento.artistas;
+    if (evento) {
+      document.getElementById("tituloEvento").textContent = evento.titulo;
+      document.getElementById("localEvento").textContent = evento.local;
+      document.getElementById("precoEvento").textContent = evento.preco;
+      document.getElementById("tipoEvento").textContent = evento.tipo;
+      document.getElementById("artistasEvento").textContent = evento.artistas.join(", ");
+    } else {
+      document.getElementById("tituloEvento").textContent = "Evento não encontrado.";
+    }
   })
   .catch(error => {
     document.getElementById("tituloEvento").textContent = "Erro ao carregar evento.";
