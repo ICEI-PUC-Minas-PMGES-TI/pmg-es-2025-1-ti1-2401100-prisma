9508f168f1211225e29c9f92c7500f22c1db0444
lucassts5 <lucas123edu78@gmail.com>
2025-06-28 00:36:00 -0300

    .

--- codigo/lucas-eduardo/events.js
+++ codigo/lucas-eduardo/events.js
@@ -1,19 +1,15 @@
+// url JSON de eventos.
 fetch("eventos.json")
   .then(response => response.json())
   .then(data => {
-    const params = new URLSearchParams(window.location.search);
-    const id = params.get("id");
-    const evento = data.find(e => e.id == id);
+    
+    const evento = data[0];
 
-    if (evento) {
-      document.getElementById("tituloEvento").textContent = evento.titulo;
-      document.getElementById("localEvento").textContent = evento.local;
-      document.getElementById("precoEvento").textContent = evento.preco;
-      document.getElementById("tipoEvento").textContent = evento.tipo;
-      document.getElementById("artistasEvento").textContent = evento.artistas.join(", ");
-    } else {
-      document.getElementById("tituloEvento").textContent = "Evento não encontrado.";
-    }
+    document.getElementById("tituloEvento").textContent = evento.titulo;
+    document.getElementById("localEvento").textContent = evento.local;
+    document.getElementById("precoEvento").textContent = evento.preco;
+    document.getElementById("tipoEvento").textContent = evento.tipo;
+    document.getElementById("artistasEvento").textContent = evento.artistas;
   })
   .catch(error => {
     document.getElementById("tituloEvento").textContent = "Erro ao carregar evento.";
